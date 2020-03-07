15cd7006350c70a790bcfa0ba40c7a9c220881d6
Rahul Barhate <rahulbarhate97@gmail.com>
2020-03-07 16:54:57 +0530

    Add files via upload

---
+++ DFS/graph.java
@@ -0,0 +1,66 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class graph
+{
+
+	private int V;
+	private LinkedList<Integer> adj[];
+	
+	
+	public graph(int v)
+	{
+		V = v;
+		adj = new LinkedList[V];
+		for(int i=0; i<V; i++)
+		{
+			adj[i] = new LinkedList<Integer>();
+		}
+	}
+	
+	void addEdge(int v, int w)
+	{
+		adj[v].add(w);
+	}
+	
+	void DFSUtil(int v, boolean visited[])
+	{
+		visited[v] = true;
+		System.out.println(v + "");
+		
+		Iterator<Integer> i = adj[v].listIterator();
+		while(i.hasNext())
+		{
+			int n = i.next();
+			if(!visited[n])
+			{
+				DFSUtil(n, visited);
+			}
+		}
+		
+	}
+	
+	void DFS(int v)
+	{
+		boolean visited[] = new boolean[V];
+		visited[v] = true;
+		
+		DFSUtil(v, visited);
+	}
+
+	public static void main(String[] args)
+	{
+		graph g = new graph(4);
+		
+		g.addEdge(0,1);
+		g.addEdge(0,2);
+		g.addEdge(1,2);
+		g.addEdge(2,0);
+		g.addEdge(2,3);
+		g.addEdge(3,3);
+		
+		g.DFS(2);
+
+	}
+
+}
